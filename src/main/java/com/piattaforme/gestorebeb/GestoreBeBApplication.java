b59aff4c23052b6c875c86678dbacfc5cdfddd4d
umbertofrega <umbertofrega@gmail.com>
2025-11-16 18:03:17 +0100

    project init
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/piattaforme/gestorebeb/GestoreBeBApplication.java
@@ -0,0 +1,13 @@
+package com.piattaforme.gestorebeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoreBeBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestoreBeBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piattaforme/gestorebeb/GestoreBeBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piattaforme.gestorebeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoreBeBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
