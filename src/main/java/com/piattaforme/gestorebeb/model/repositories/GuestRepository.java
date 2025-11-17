487ad31d005d6b268e19179275393a8ecd7f3f25
umbertofrega <umbertofrega@gmail.com>
2025-11-17 11:53:25 +0100

    added new queries

--- src/main/java/com/piattaforme/gestorebeb/model/repositories/GuestRepository.java
+++ src/main/java/com/piattaforme/gestorebeb/model/repositories/GuestRepository.java
@@ -2,6 +2,8 @@ package com.piattaforme.gestorebeb.model.repositories;
 
 import com.piattaforme.gestorebeb.model.entities.Guest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface GuestRepository extends JpaRepository<Guest,Integer> {
+    List<Guest> findGuestByEmail(String email);
 }
