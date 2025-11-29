38d84a62fad19e418b92926a74a5062643afde39
umbertofrega <umbertofrega@gmail.com>
2025-11-29 18:42:31 +0100

    new operation

--- src/main/java/com/piattaforme/gestorebeb/model/repositories/AdminRepository.java
+++ src/main/java/com/piattaforme/gestorebeb/model/repositories/AdminRepository.java
@@ -8,4 +8,8 @@ import java.util.List;
 public interface AdminRepository extends JpaRepository<Admin,Integer> {
     List<Admin> findAdminByEmail(String email);
     boolean existsByEmail(String email);
+
+    Admin searchById(int adminId);
+
+    Admin getAdminById(int ownerId);
 }
