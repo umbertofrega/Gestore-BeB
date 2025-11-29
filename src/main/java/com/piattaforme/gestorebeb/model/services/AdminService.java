c164895b580776aa2bce9f2c945cfc4634df07ae
umbertofrega <umbertofrega@gmail.com>
2025-11-29 19:08:24 +0100

    clarified exception

--- src/main/java/com/piattaforme/gestorebeb/model/services/AdminService.java
+++ src/main/java/com/piattaforme/gestorebeb/model/services/AdminService.java
@@ -3,7 +3,7 @@ package com.piattaforme.gestorebeb.model.services;
 import com.piattaforme.gestorebeb.model.entities.Admin;
 import com.piattaforme.gestorebeb.model.enums.AdminRole;
 import com.piattaforme.gestorebeb.model.exceptions.conflict.EmailAlreadyExists;
-import com.piattaforme.gestorebeb.model.exceptions.notFound.UserNotFoundException;
+import com.piattaforme.gestorebeb.model.exceptions.forbidden.InsufficientRoleException;
 import com.piattaforme.gestorebeb.model.repositories.AdminRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,25 +20,16 @@ public class AdminService {
     @Transactional
     public Admin addAdmin(Admin newAdmin, int ownerId){
         if(newAdmin == null)
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("The new admin can't be null");
 
         if(adminRepository.existsByEmail(newAdmin.getEmail()))
             throw new EmailAlreadyExists("Used email");
         Admin owner = adminRepository.getAdminById(ownerId);
 
-        if(!owner.getRole().equals(AdminRole.OWNER))
-            throw new IllegalArgumentException();
-
+        if (!owner.getRole().equals(AdminRole.OWNER)) {
+            System.err.print("The admin " + owner + " tried to do an owner operation.");
+            throw new InsufficientRoleException("Only the owner can do this operation, you are not the owner");
+        }
         return adminRepository.save(newAdmin);
-    }
-
-    public Admin changeRole(int adminId, AdminRole role){
-        Admin admin = adminRepository.searchById(adminId);
-
-        if(admin == null)
-            throw new UserNotFoundException("This admin doesn't exist");
-
-        admin.setRole(role);
-        return adminRepository.save(admin);
     }
 }
