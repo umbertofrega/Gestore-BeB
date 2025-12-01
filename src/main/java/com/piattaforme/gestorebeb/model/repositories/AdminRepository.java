1b10a1de59355a3440a072f41824589a016f060a
umbertofrega <umbertofrega@gmail.com>
2025-12-01 17:53:05 +0100

    added AdminController

--- src/main/java/com/piattaforme/gestorebeb/model/repositories/AdminRepository.java
+++ src/main/java/com/piattaforme/gestorebeb/model/repositories/AdminRepository.java
@@ -3,13 +3,8 @@ package com.piattaforme.gestorebeb.model.repositories;
 import com.piattaforme.gestorebeb.model.entities.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface AdminRepository extends JpaRepository<Admin,Integer> {
-    List<Admin> findAdminByEmail(String email);
     boolean existsByEmail(String email);
 
-    Admin searchById(int adminId);
-
-    Admin getAdminById(int ownerId);
+    Admin getAdminByCode(String code);
 }
--- src/main/java/com/piattaforme/gestorebeb/model/services/AdminService.java
+++ src/main/java/com/piattaforme/gestorebeb/model/services/AdminService.java
@@ -5,8 +5,13 @@ import com.piattaforme.gestorebeb.model.enums.AdminRole;
 import com.piattaforme.gestorebeb.model.exceptions.conflict.EmailAlreadyExists;
 import com.piattaforme.gestorebeb.model.exceptions.forbidden.InsufficientRoleException;
 import com.piattaforme.gestorebeb.model.repositories.AdminRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class AdminService {
@@ -18,18 +23,32 @@ public class AdminService {
     }
 
     @Transactional
-    public Admin addAdmin(Admin newAdmin, int ownerId){
+    public Admin addAdmin(Admin newAdmin) {
         if(newAdmin == null)
             throw new IllegalArgumentException("The new admin can't be null");
 
         if(adminRepository.existsByEmail(newAdmin.getEmail()))
             throw new EmailAlreadyExists("Used email");
-        Admin owner = adminRepository.getAdminById(ownerId);
+
+        isRealOwner();
+
+        return adminRepository.save(newAdmin);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Admin> getAll() {
+        isRealOwner();
+        return adminRepository.findAll();
+    }
+
+    private void isRealOwner() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String ownerKeycloakCode = ((Jwt) authentication.getPrincipal()).getSubject();
+        Admin owner = adminRepository.getAdminByCode(ownerKeycloakCode);
 
         if (!owner.getRole().equals(AdminRole.OWNER)) {
             System.err.print("The admin " + owner + " tried to do an owner operation.");
             throw new InsufficientRoleException("Only the owner can do this operation, you are not the owner");
         }
-        return adminRepository.save(newAdmin);
     }
 }
