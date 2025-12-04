a3d78f1b9d013356b7abb99efbd14bdea3dc2b1f
umbertofrega <umbertofrega@gmail.com>
2025-12-04 23:52:05 +0100

    removed useless query

--- src/main/java/com/piattaforme/gestorebeb/model/repositories/UserRepository.java
+++ src/main/java/com/piattaforme/gestorebeb/model/repositories/UserRepository.java
@@ -3,6 +3,5 @@ package com.piattaforme.gestorebeb.model.repositories;
 import com.piattaforme.gestorebeb.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByCode(String code);
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
