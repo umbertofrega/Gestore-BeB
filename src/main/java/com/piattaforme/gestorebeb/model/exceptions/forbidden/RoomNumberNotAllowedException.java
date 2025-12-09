61a79dbb8660ca4bda94630c0c9b82f5253d8c5e
umbertofrega <umbertofrega@gmail.com>
2025-12-09 17:20:52 +0100

    removed useless exception

--- src/main/java/com/piattaforme/gestorebeb/model/exceptions/forbidden/RoomNumberNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.piattaforme.gestorebeb.model.exceptions.forbidden;
-
-public class RoomNumberNotAllowedException extends ForbiddenException {
-    public RoomNumberNotAllowedException(String message) {
-        super(message);
-    }
-}
